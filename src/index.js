27c781e95cb1b4b4429a309057f0eabb396445e6
Ong Zhi Zheng <Fogeinator@users.noreply.github.com>
2020-09-19 23:41:37 +0800

    add quotes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const cool = require('cool-ascii-faces');
+const quotes = require('inspirational-quotes');
 
 module.exports = async function App(context) {
   if (context.event.isText) {
-    await context.sendText(cool());
+    await context.sendText(quotes.getRandomQuote() + '\n' + '~ ' + cool());
   }
 };
